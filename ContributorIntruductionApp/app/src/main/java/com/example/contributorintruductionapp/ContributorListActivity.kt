d837265719eca012d6b293c0a3419f805be9a449
matsubara_mi <floods_of_tears@hotmail.co.jp>
2021-02-14 01:02:06 +0900

    add: データ取得処理追加（確認用としてtextViewに表示）

--- ContributorIntruductionApp/app/src/main/java/com/example/contributorintruductionapp/ContributorListActivity.kt
+++ ContributorIntruductionApp/app/src/main/java/com/example/contributorintruductionapp/ContributorListActivity.kt
@@ -1,19 +1,48 @@
 package com.example.contributorintruductionapp
 
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Gravity
+import android.widget.LinearLayout
 import android.widget.TextView
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
+import okhttp3.*
+import java.io.IOException
 
-class ContributorListActivity : AppCompatActivity() {
+class ContributorListActivity<JSonObject> : AppCompatActivity() {
+    private val client:OkHttpClient = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contributor_list)
+
+        val url:String = "https://api.github.com/repos/googlesamples/android-architecture-components/contributors"
+        val body: FormBody = FormBody.Builder().build()
+        val handler = Handler(Looper.getMainLooper())
+        val textView: TextView = findViewById<TextView>(R.id.textView)
+
+        getContributorData(url, handler, textView)
+    }
+
+    private fun getContributorData (url: String, handler: Handler, textView: TextView) {
+        val request = Request.Builder().url(url).build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("fail : $e")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if(response.isSuccessful) {
+                    response.body()?.let {
+                        val responseText: String? = response.body()?.string()
+                        handler.post() {
+                            textView.text = responseText
+                        }
+                    }
+                }
+            }
+        })
     }
 }
